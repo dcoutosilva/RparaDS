e5a8db97f18d84631f37d6a99a903897bcc769af
Danilo Silva <d.couto.silva@gmail.com>
2023-12-14 09:27:06 -0300

    cap2

---
+++ cap2.R
@@ -0,0 +1,25 @@
+#basic programming
+
+1/200*30
+
+(59+73+2)/3
+
+sin(pi/2)
+
+x <- 3*4
+#alt - <- 
+x
+esteÉUmNomeMuitoLongo <- 2.5
+
+r_rocks <- 2^3
+r_rocks
+
+library(tidyverse)
+
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(
+    x = displ, y = hwy
+  ))
+filter(mpg, cyl = 8)
+filter(diadond, carat >3)
+
