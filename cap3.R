f7697a81746f9b6aff4c8d7de04d1a066f3dfcfd
Danilo Silva <d.couto.silva@gmail.com>
2023-12-26 11:18:02 -0300

    pg58

--- cap3.R
+++ cap3.R
@@ -223,3 +223,80 @@ select(flights, one_of(vars))
 select(flights, contains("TIME"))
 
 
+
+#adicionar novas variaveis com mutate()
+
+flights_sml <- 
+  select(
+    flights, year:day, ends_with("delay"), distance, air_time
+  )
+mutate(flights_sml,
+       gain = arr_delay - dep_delay,
+       speed = distance / air_time * 60
+       )
+
+#criação de novas variaveis, como gain e speed, já calculando respectivamente
+
+mutate(flights_sml,
+       gain = arr_delay - dep_delay,
+       hours = air_time / 60,
+       gain_per_hour = gain / hours
+       )
+
+transmute(
+  flights,
+  gain = arr_delay - dep_delay,
+  hours = air_time / 60,
+  gain_per_hours = gain / hours
+)#apenas manter as novas variaveis, porém não se altera na original
+
+view(flights)
+
+
+#funções de criação uteis
+
+transmute(flights,
+          dep_time,
+          hour = dep_time %/% 100,
+          minute = dep_time %% 100
+          )
+#%/%  divisão inteira
+#%% divisão apenas resto
+
+#log()
+#log2()
+#log10()
+
+#offsets
+
+#lead()
+#lag()
+
+x <- 1:10
+lag(x)
+lead(x)
+x != lag(x)
+x - lag(x)
+x - lead(x)
+
+cumsum(x)#somatoria da variavel x
+
+cummean(x) #somatória da media da variavel x
+
+#classificação
+
+y <-  c(1,2,2,NA,3,4)
+min_rank(y)
+min_rank(desc(y))
+row_number(y)
+dense_rank(y)
+percent_rank(y)
+cume_dist(y)
+ntile(y)
+
+
+#exer1 pg58
+
+
+
+
