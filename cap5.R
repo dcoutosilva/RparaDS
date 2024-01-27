67178fa513c8b718230b72db8308cac10c512ea3
Danilo Silva <d.couto.silva@gmail.com>
2024-01-27 12:01:51 -0300

    commit

--- cap5.R
+++ cap5.R
@@ -95,7 +95,92 @@ nycflights13::flights %>%
 
 #covariação
 
-#geom_
+#geom_freqpoly não é tão util para comparações., porque a altura é dada pelo count
+#se uns dos grupos for muito menor do que os outros, fica dificil de ver as 
+#diferenças, vamos ver no exeomplo abaixo
+
+ggplot(
+  data = diamonds,
+  mapping = aes(
+    x = price
+  )
+) +
+  geom_freqpoly(
+    mapping = aes(
+      color = cut
+    ), binwidth = 500
+  )
+
+#para melhorar a comparação, vamos mudar o eixo y
+#em vez de count, exibiremos density que é a contagem padronizada para que a area
+#de cada poligono de frequencia 
+
+ggplot(
+  data=diamonds,
+  mapping = aes(
+    x = price,
+    y = ..density..
+  )
+) +
+  geom_freqpoly(
+    mapping = aes(
+      color = cut
+    ), binwidth = 500
+  )
+#parece que diamantes fair(justos) tem o preço medio mais alto, isso porque seja 
+#os poligonos de frequencia seja mais dificil de interpretar
+#veremos a diferença em um boxplot
+
+ggplot(
+  data=diamonds,
+  mapping = aes(
+    x = cut, y = price
+  )
+) +
+  geom_boxplot()
+#você tem bem mais informações detalhadas, assim fazendo uma analise melhor.
+
+#vamos trazer outro exemplo
+
+ggplot(
+  data=mpg,
+  mapping = aes(
+    x = class,
+    y = hwy
+  )
+) +
+  geom_boxplot()
+
+ggplot(
+  data=mpg
+) +
+  geom_boxplot(
+    mapping = aes(
+      x = reorder(
+        class,
+        hwy,
+        FUN = median
+      ),
+      y = hwy
+    )
+  ) + 
+  coord_flip()
 
 
+#exer1 #pg 99
+
+nycflights13::flights %>% 
+  mutate(
+    cancelados = is.na(dep_time),
+    agend_hora = sched_dep_time %/% 100,
+    agend_min = sched_dep_time %% 100,
+    agen_hora_min = agend_hora + agend_min /60 
+    
+  ) %>% 
+  ggplot() +
+  geom_boxplot(
+    mapping = aes(
+      x = cancelados, y = agen_hora_min
+    )
+  )
 
