74ed2ae859b505bf9dd53de3578aa733746fac75
dcoutosilva <d.couto.silva@gmail.com>
2024-11-02 10:01:49 -0300

    studying

--- shiny.R
+++ shiny.R
@@ -14,9 +14,37 @@ for(p in pack){
 #função chamada server
 #chamada da função shinyApp()
 
-ui <- fluidPage("Olá Mundo")
+ui <- fluidPage("Histograma da Variável mpg",
+                selectInput(
+                  inputId = "variavel",
+                  label = "Selecione a variável",
+                  choices = names(mtcars)
+                ),
+                plotOutput(outputId = "histograma")
+                )
 server <- function(input, output, session){
   #sempre recebe input, output, session
+  
+  #input <- enviar informações ao servidor
+  #output <- devolvidos e processados por ela
+  
+  #output()
+  #plotOutput() <- graficos
+  #tableOutput() <- tabelas
+  #textOutput() <- texto
+  #uiOutput() <- html
+  #eles especificam onde serão criados e não como.
+  
+  #render, ele renderiza os resultados para html
+  #renderPlot()
+  #renderTable()
+  #renderText()
+  #rendeUI()
+  output$histograma <- renderPlot({
+    hist(mtcars[[input$variavel]])
+  })
+  
+  
   
 }
 shinyApp(ui, server)#junta as duas funções, sempre no final
